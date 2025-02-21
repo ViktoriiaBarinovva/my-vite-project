1ff8cc1757d7bc5780d8320a2fa008d2e7419b3c
ViktoriiaBarinovva <barryviktoriia@gmail.com>
2025-02-21 13:48:54 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-vite-project/', // Это должно быть названием репозитория на GitHub
-  build: {
-    outDir: 'dist', // Папка, куда собираются файлы
-  },
+  plugins: [react()],
+  base: '/qr-code'
 })
